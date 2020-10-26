83c383da24a73220b72baf2d6869362586541c84
lcd1232 <8745863+lcd1232@users.noreply.github.com>
2020-10-26 17:03:53 +0300

    Add method with auth

--- client.go
+++ client.go
@@ -3,7 +3,9 @@ package paddle
 import "net/http"
 
 type Client struct {
-	client *http.Client
+	client         *http.Client
+	vendorID       string
+	vendorAuthCode string
 }
 
 func NewClient(client *http.Client) *Client {
@@ -13,4 +15,11 @@ func NewClient(client *http.Client) *Client {
 	return &Client{
 		client: client,
 	}
+}
+
+func NewClientWithAuthentication(client *http.Client, vendorID string, vendorAuthCode string) *Client {
+	c := NewClient(client)
+	c.vendorID = vendorID
+	c.vendorAuthCode = vendorAuthCode
+	return c
 }
--- client_test.go
+++ client_test.go
@@ -11,4 +11,14 @@ func TestNewClient(t *testing.T) {
 	c := NewClient(nil)
 	require.NotNil(t, c)
 	assert.NotNil(t, c.client)
+	assert.Zero(t, c.vendorID)
+	assert.Zero(t, c.vendorAuthCode)
+}
+
+func TestNewClientWithAuthentication(t *testing.T) {
+	c := NewClientWithAuthentication(nil, "id1", "auth1")
+	require.NotNil(t, c)
+	assert.NotNil(t, c.client)
+	assert.Equal(t, "id1", c.vendorID)
+	assert.Equal(t, "auth1", c.vendorAuthCode)
 }
