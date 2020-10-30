e601c0efef63c5c57ff252c33da8552d8e215bc2
Patrik Hjortshøj Lindberg <patrikhlindberg@gmail.com>
2020-10-31 00:07:47 +0100

    Add Transfer Created webhook

---
+++ webhook_transfer_created.go
@@ -0,0 +1,50 @@
+package paddle
+
+import (
+	"net/url"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+func (c *Client) ParseTransferCreatedWebhook(form url.Values) (TransferCreated, error) {
+	signature := form.Get(signatureKey)
+	if err := c.verifier.Verify(c.publicKey, signature, form); err != nil {
+		return TransferCreated{}, errors.WithStack(err)
+	}
+	var tcw transferCreatedWebhook
+	if err := decoder.Decode(&tcw, form); err != nil {
+		return TransferCreated{}, errors.WithStack(err)
+	}
+	tc := TransferCreated{
+		AlertName: Alert(tcw.AlertName),
+		AlertID:   tcw.AlertID,
+		Amount:    tcw.Amount,
+		Currency:  tcw.Currency,
+		EventTime: time.Time(tcw.EventTime),
+		PayoutID:  tcw.PayoutID,
+		Status:    Status(tcw.Status),
+	}
+	return tc, nil
+}
+
+type TransferCreated struct {
+	AlertName Alert
+	AlertID   string
+	Amount    string
+	Currency  string
+	EventTime time.Time
+	PayoutID  string
+	Status    Status
+}
+
+type transferCreatedWebhook struct {
+	AlertName string     `schema:"alert_name"`
+	AlertID   string     `schema:"alert_id"`
+	Amount    string     `schema:"amount"`
+	Currency  string     `schema:"currency"`
+	EventTime customTime `schema:"event_time"`
+	PayoutID  string     `schema:"payout_id"`
+	Status    string     `schema:"status"`
+	Signature string     `schema:"p_signature"`
+}
